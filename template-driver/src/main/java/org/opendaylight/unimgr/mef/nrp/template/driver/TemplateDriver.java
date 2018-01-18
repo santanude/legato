aa02549683f39b4dd17aa11119526cefd559ee85
santanu.de <santanu.de@xoriant.com>
2018-01-18 06:57:49 +0100

    first commit

---
+++ template-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/template/driver/TemplateDriver.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2017 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.unimgr.mef.nrp.template.driver;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.unimgr.mef.nrp.api.ActivationDriver;
+import org.opendaylight.unimgr.mef.nrp.api.ActivationDriverBuilder;
+import org.opendaylight.unimgr.mef.nrp.api.EndPoint;
+import org.opendaylight.unimgr.mef.nrp.common.ResourceActivatorException;
+import org.opendaylight.unimgr.mef.nrp.template.TemplateConstants;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrp._interface.rev171221.NrpConnectivityServiceAttrs;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.Uuid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Example driver builder
+ * @author bartosz.michalik@amartus.com
+ */
+public class TemplateDriver implements ActivationDriverBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(TemplateDriver.class);
+    @Override
+    public Optional<ActivationDriver> driverFor(BuilderContext context) {
+        // build a stateful driver
+        // the API contract is following
+        // 1. initialize is called
+        // 2. activate or deactivate metod is called depending on NRP call (update will be supported soon)
+        // 3. if activation/deactivation succeeds for all drivers the commit metod is called
+        // 3a. if activation/deactivation fails for any driver rollback is called
+        return Optional.of(new ActivationDriver() {
+
+            public String serviceId;
+
+            @Override
+            public void commit() {
+                LOG.info("commit was triggered for {}", serviceId);
+            }
+
+            @Override
+            public void rollback() {
+                LOG.info("rollback was triggered for {}", serviceId);
+            }
+
+            @Override
+            public void initialize(List<EndPoint> endPoints, String serviceId, NrpConnectivityServiceAttrs context) {
+                this.serviceId = serviceId;
+            }
+
+            @Override
+            public void activate() throws TransactionCommitFailedException, ResourceActivatorException {
+                // method can fail if you wish
+                LOG.info("activate was triggered for {}", serviceId);
+            }
+
+            @Override
+            public void deactivate() throws TransactionCommitFailedException, ResourceActivatorException {
+                // method can fail if you wish
+                LOG.info("dectivate was triggered for {}", serviceId);
+            }
+
+            @Override
+            public void update() throws TransactionCommitFailedException, ResourceActivatorException {
+
+            }
+
+            @Override
+            public int priority() {
+                //if you would like to make your driver first on the list
+
+                return 0;
+            }
+        });
+    }
+
+    @Override
+    public Uuid getNodeUuid() {
+        return new Uuid(TemplateConstants.DRIVER_ID);
+    }
+}
---
+++ template-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/template/tapi/TopologyDataHandler.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2017 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.unimgr.mef.nrp.template.tapi;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.unimgr.mef.nrp.api.TopologyManager;
+import org.opendaylight.unimgr.mef.nrp.common.NrpDao;
+import org.opendaylight.unimgr.mef.nrp.common.TapiUtils;
+import org.opendaylight.unimgr.mef.nrp.template.TemplateConstants;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.mef.common.types.rev171221.NaturalNumber;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrp._interface.rev171221.ServiceInterfacePoint1;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrp._interface.rev171221.ServiceInterfacePoint1Builder;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrp._interface.rev171221.nrp.sip.attrs.NrpCarrierEthUniNResourceBuilder;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.ETH;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.PortDirection;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.PortRole;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.Uuid;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.context.attrs.ServiceInterfacePoint;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.common.rev171113.context.attrs.ServiceInterfacePointBuilder;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.topology.rev171113.node.OwnedNodeEdgePoint;
+import org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.tapi.topology.rev171113.node.OwnedNodeEdgePointBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author bartosz.michalik@amartus.com
+ */
+public class TopologyDataHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(TopologyDataHandler.class);
+    private DataBroker dataBroker;
+    private TopologyManager topologyManager;
+
+    public TopologyDataHandler(DataBroker dataBroker, TopologyManager topologyManager) {
+        this.dataBroker = dataBroker;
+        this.topologyManager = topologyManager;
+    }
+
+    public void init() {
+        Objects.requireNonNull(dataBroker);
+        LOG.info("Starting topology handler");
+        // this is a static and simplistic topology push to the TAPI system topology
+
+        ReadWriteTransaction tx = dataBroker.newReadWriteTransaction();
+
+        try {
+
+            // we have prepared an dao abstraction to make it easier to use some of the common interactions with
+            // MD-SAL but you can use tx.put tx.merge etc. by yourself if you prefere to
+            NrpDao nrpDao = new NrpDao(tx);
+            //we are creating a list of NodeEdgePoints for the node no sips are added to the system
+            List<OwnedNodeEdgePoint> someEndpoints = createSomeEndpoints(1, 2, 5, 7);
+            nrpDao.createNode(topologyManager.getSystemTopologyId(), TemplateConstants.DRIVER_ID, ETH.class, null);
+            //add sip for one of these endpoints
+
+            //create sid and add it to model
+            ServiceInterfacePoint someSip1 = createSomeSip("some-sip-1");
+            ServiceInterfacePoint someSip2 = createSomeSip("some-sip-2");
+            nrpDao.addSip(someSip1);
+            nrpDao.addSip(someSip2);
+
+            //update an existing nep with mapping to sip
+            OwnedNodeEdgePoint updatedNep1 = new OwnedNodeEdgePointBuilder(someEndpoints.get(1))
+                    .setMappedServiceInterfacePoint(Collections.singletonList(someSip1.getUuid()))
+                    .build();
+
+            OwnedNodeEdgePoint updatedNep2 = new OwnedNodeEdgePointBuilder(someEndpoints.get(2))
+                    .setMappedServiceInterfacePoint(Collections.singletonList(someSip2.getUuid()))
+                    .build();
+
+            nrpDao.updateNep(TemplateConstants.DRIVER_ID, updatedNep1);
+            nrpDao.updateNep(TemplateConstants.DRIVER_ID, updatedNep2);
+
+
+            tx.submit().checkedGet();
+        } catch (TransactionCommitFailedException e) {
+            LOG.error("Adding node to system topology has failed", e);
+        }
+
+    }
+
+    private ServiceInterfacePoint createSomeSip(String idx) {
+        ServiceInterfacePoint1 sip = new ServiceInterfacePoint1Builder()
+                .setNrpCarrierEthUniNResource(
+                        new NrpCarrierEthUniNResourceBuilder()
+                                .setMaxFrameSize(new NaturalNumber(new Long(1703)))
+                                .build()
+                ).build();
+
+        return new ServiceInterfacePointBuilder()
+                .setUuid(new Uuid("sip" + ":" + TemplateConstants.DRIVER_ID + ":" + idx))
+                .setLayerProtocol(Collections.singletonList(TapiUtils.toSipPN(ETH.class)))
+                .addAugmentation(ServiceInterfacePoint1.class, sip)
+                .build();
+    }
+
+    private List<OwnedNodeEdgePoint> createSomeEndpoints(int... indexes) {
+
+        return Arrays.stream(indexes).mapToObj(idx -> new OwnedNodeEdgePointBuilder()
+                .setUuid(new Uuid(TemplateConstants.DRIVER_ID + ":nep" + idx))
+                .setLayerProtocol(Collections.singletonList(TapiUtils.toNepPN(ETH.class)))
+                .setLinkPortDirection(PortDirection.BIDIRECTIONAL)
+                .setLinkPortRole(PortRole.SYMMETRIC)
+
+                .build()).collect(Collectors.toList());
+    }
+
+    public void close() {
+        LOG.info("Closing topology handler");
+    }
+
+}
