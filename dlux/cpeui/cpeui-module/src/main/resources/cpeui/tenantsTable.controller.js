3dc58851c4ecafb8f77e77378e00879ab864805b
santanu.de <santanu.de@xoriant.com>
2018-01-18 06:36:50 +0100

    first commit

---
+++ dlux/cpeui/cpeui-module/src/main/resources/cpeui/tenantsTable.controller.js
@@ -0,0 +1,6 @@
+define([ 'app/cpeui/cpeui.module' ], function(cpeui) {
+
+  cpeui.register.controller('TenantTableCtrl', function($scope, CpeuiSvc, CpeuiDialogs, $mdDialog, $mdMedia) {
+
+  });
+});
