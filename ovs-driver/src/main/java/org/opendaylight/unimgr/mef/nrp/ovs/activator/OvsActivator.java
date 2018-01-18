ee34f24e87822f2c5615aa83f174fca2d568680c
santanu.de <santanu.de@xoriant.com>
2018-01-18 06:54:28 +0100

    first commit

---
+++ ovs-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/ovs/activator/OvsActivator.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.ovs.activator;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.unimgr.mef.nrp.api.EndPoint;
+import org.opendaylight.unimgr.mef.nrp.common.ResourceActivator;
+import org.opendaylight.unimgr.mef.nrp.common.ResourceNotAvailableException;
+import org.opendaylight.unimgr.mef.nrp.ovs.transaction.TableTransaction;
+import org.opendaylight.unimgr.mef.nrp.ovs.transaction.TopologyTransaction;
+import org.opendaylight.unimgr.mef.nrp.ovs.util.OpenFlowUtils;
+import org.opendaylight.unimgr.mef.nrp.ovs.util.OvsdbUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author marek.ryznar@amartus.com
+ */
+public class OvsActivator implements ResourceActivator {
+
+    private final DataBroker dataBroker;
+    private static final Logger LOG = LoggerFactory.getLogger(OvsActivator.class);
+
+    //TODO introduce poll synced with ovsdb config
+    private static final AtomicInteger queueNumberGenerator = new AtomicInteger(200);
+
+    public OvsActivator(DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    /**
+     * Set state for the driver for a (de)activation transaction.
+     * @param endPoints list of endpoint to interconnect
+     */
+    @Override
+    public void activate(List<EndPoint> endPoints, String serviceName) throws ResourceNotAvailableException, TransactionCommitFailedException {
+        for (EndPoint endPoint:endPoints) {
+            activateEndpoint(endPoint, serviceName);
+        }
+
+    }
+
+
+    private void activateEndpoint(EndPoint endPoint, String serviceName) throws ResourceNotAvailableException, TransactionCommitFailedException {
+        // Transaction - Get Open vSwitch node and its flow table
+        String portName = OvsActivatorHelper.getPortName(endPoint.getEndpoint().getServiceInterfacePoint().getValue());
+        TopologyTransaction topologyTransaction = new TopologyTransaction(dataBroker);
+        Node node = topologyTransaction.readNode(portName);
+        Table table = OpenFlowUtils.getTable(node);
+
+        // Prepare list of flows to be added/removed
+        List<Flow> flowsToWrite = new ArrayList<>();
+        List<Flow> flowsToDelete = new ArrayList<>();
+        List<Link> interswitchLinks = topologyTransaction.readInterswitchLinks(node);
+        if (!OpenFlowUtils.isTablePreconfigured(table)) {
+            LOG.debug("Table is not preconfigured. Adding base flows.");
+            flowsToWrite.addAll(OpenFlowUtils.getBaseFlows(interswitchLinks));
+            flowsToDelete.addAll(OpenFlowUtils.getExistingFlows(table));
+        }
+
+        OvsActivatorHelper ovsActivatorHelper = new OvsActivatorHelper(topologyTransaction, endPoint);
+        String openFlowPortName = ovsActivatorHelper.getOpenFlowPortName();
+        long queueNumber = queueNumberGenerator.getAndIncrement();
+        int internalVlanId = ovsActivatorHelper.getInternalVlanId();
+        flowsToWrite.addAll(OpenFlowUtils.getVlanFlows(openFlowPortName, internalVlanId, interswitchLinks, serviceName, queueNumber));
+
+        // Transaction - Add flows related to service to table and remove unnecessary flows
+        TableTransaction tableTransaction = new TableTransaction(dataBroker, node, table);
+        tableTransaction.deleteFlows(flowsToDelete, true);
+        tableTransaction.writeFlows(flowsToWrite);
+
+		List<String> outputPortNames = interswitchLinks.stream()
+				.map(link -> ovsActivatorHelper.getTpNameFromOpenFlowPortName(link.getLinkId().getValue()))
+				.collect(Collectors.toList());
+
+		if(ovsActivatorHelper.isIBwpConfigured()) {
+            //Create egress qos
+            OvsdbUtils.createEgressQos(dataBroker, portName, outputPortNames, ovsActivatorHelper.getQosMinRate(),
+                    ovsActivatorHelper.getQosMaxRate(), serviceName, queueNumber);
+        }
+
+
+
+    }
+
+	@Override
+    public void deactivate(List<EndPoint> endPoints, String serviceName) throws TransactionCommitFailedException, ResourceNotAvailableException {
+
+        for (EndPoint endPoint:endPoints) {
+        	deactivateEndpoint(endPoint, serviceName);
+        }
+    }
+
+    private void deactivateEndpoint(EndPoint endPoint, String serviceName) throws ResourceNotAvailableException, TransactionCommitFailedException {
+
+        // Transaction - Get Open vSwitch node and its flow table
+        TopologyTransaction topologyTransaction = new TopologyTransaction(dataBroker);
+        OvsActivatorHelper ovsActivatorHelper = new OvsActivatorHelper(topologyTransaction, endPoint);
+
+        Node openFlowNode = topologyTransaction.readNodeOF(ovsActivatorHelper.getOpenFlowPortName());
+        Table table = OpenFlowUtils.getTable(openFlowNode);
+        // Get list of flows to be removed
+        List<Flow> flowsToDelete = OpenFlowUtils.getServiceFlows(table, serviceName);
+
+        // Transaction - Remove flows related to service from table
+        TableTransaction tableTransaction = new TableTransaction(dataBroker, openFlowNode, table);
+        tableTransaction.deleteFlows(flowsToDelete, false);
+
+        String portName = OvsActivatorHelper.getPortName(endPoint.getEndpoint().getServiceInterfacePoint().getValue());
+        Node node = topologyTransaction.readNode(portName);
+
+        //list with endpoint + all interswitch ports
+    	List<String> tpsWithQos = topologyTransaction.readInterswitchLinks(node).stream()
+				.map(link -> ovsActivatorHelper.getTpNameFromOpenFlowPortName(link.getLinkId().getValue()))
+				.collect(Collectors.toList());
+    	tpsWithQos.add(portName);
+
+        OvsdbUtils.removeQosEntryFromTerminationPoints(dataBroker, serviceName, tpsWithQos);
+    }
+
+	public void update(List<EndPoint> endPoints, String serviceName) throws ResourceNotAvailableException, TransactionCommitFailedException {
+        for (EndPoint endPoint:endPoints) {
+            updateEndpoint(endPoint, serviceName);
+        }
+	}
+
+	private void updateEndpoint(EndPoint endPoint, String serviceName) throws ResourceNotAvailableException, TransactionCommitFailedException{
+
+		TopologyTransaction topologyTransaction = new TopologyTransaction(dataBroker);
+	    OvsActivatorHelper ovsActivatorHelper = new OvsActivatorHelper(topologyTransaction, endPoint);
+
+		String portName = OvsActivatorHelper.getPortName(endPoint.getEndpoint().getServiceInterfacePoint().getValue());
+		Node node = topologyTransaction.readNode(portName);
+
+		//list with endpoint + all interswitch ports
+		List<String> interswitchPorts = topologyTransaction.readInterswitchLinks(node).stream()
+				.map(link -> ovsActivatorHelper.getTpNameFromOpenFlowPortName(link.getLinkId().getValue()))
+				.collect(Collectors.toList());
+
+		List<String> tpsWithQos = new LinkedList<>(interswitchPorts);
+		tpsWithQos.add(portName);
+
+		//remove old egress qos
+		OvsdbUtils.removeQosEntryFromTerminationPoints(dataBroker, serviceName, tpsWithQos);
+
+
+		long queueNumber = queueNumberGenerator.getAndIncrement();
+        //Create egress qos
+		OvsdbUtils.createEgressQos(dataBroker, portName, interswitchPorts, ovsActivatorHelper.getQosMinRate(),
+				ovsActivatorHelper.getQosMaxRate(), serviceName, queueNumber);
+
+		//modify flow with new queue number
+		 Table table = OpenFlowUtils.getTable(node);
+        TableTransaction tableTransaction = new TableTransaction(dataBroker, node, table);
+		tableTransaction.writeFlow(OpenFlowUtils.createVlanIngressFlow(ovsActivatorHelper.getOpenFlowPortName(), ovsActivatorHelper.getInternalVlanId(),
+				serviceName, topologyTransaction.readInterswitchLinks(node), queueNumber));
+	}
+
+}
---
+++ ovs-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/ovs/activator/OvsActivatorHelper.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.ovs.activator;
+
+import java.util.List;
+
+import org.opendaylight.unimgr.mef.nrp.api.EndPoint;
+import org.opendaylight.unimgr.mef.nrp.common.ResourceNotAvailableException;
+import org.opendaylight.unimgr.mef.nrp.ovs.exception.VlanNotSetException;
+import org.opendaylight.unimgr.mef.nrp.ovs.transaction.TopologyTransaction;
+import org.opendaylight.unimgr.utils.NullAwareDatastoreGetter;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrm.connectivity.rev171221.carrier.eth.connectivity.end.point.resource.IngressBwpFlow;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrp._interface.rev171221.nrp.connectivity.service.end.point.attrs.NrpCarrierEthConnectivityEndPointResource;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+/**
+ * Helper class for OvsDriver activation.
+ *
+ * @author jakub.niezgoda@amartus.com
+ */
+class OvsActivatorHelper {
+    private List<NullAwareDatastoreGetter<Node>> nodes;
+    private EndPoint endPoint;
+    private String tpName;
+    private BiMap<String, String> portMap;
+
+    private static final String CTAG_VLAN_ID_NOT_SET_ERROR_MESSAGE = "C-Tag VLAN Id not set for End Point '%s'.";
+    private static final String INGRESS_BWP_FLOW_NOT_SET_ERROR_MESSAGE = "Ingress bwp flow is not set for End Point '%s'.";
+    private static final String ATTRS_NOT_SET_ERROR_MESSAGE = "End Point '%s' does not have '%s' set.";
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(OvsActivatorHelper.class);
+
+    OvsActivatorHelper(TopologyTransaction topologyTransaction, EndPoint endPoint) {
+        this.nodes = topologyTransaction.readNodes();
+        this.endPoint = endPoint;
+        tpName = getPortName(endPoint.getEndpoint().getServiceInterfacePoint().getValue());
+        this.portMap = createPortMap(nodes);
+    }
+
+    /**
+     * Returns VLAN Id of the service
+     *
+     * @return Integer with VLAN Id
+     */
+    int getCeVlanId() throws ResourceNotAvailableException {
+
+        if ( (endPoint.getAttrs() != null) && (endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource() != null) ) {
+            NrpCarrierEthConnectivityEndPointResource attr = endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource();
+            if ( (attr.getCeVlanIdListAndUntag()!=null) && !(attr.getCeVlanIdListAndUntag().getVlanId().isEmpty()) ) {
+                //for now we support only one CE VLAN
+                return attr.getCeVlanIdListAndUntag().getVlanId().get(0).getVlanId().getValue().intValue();
+            } else {
+                LOG.warn(String.format(CTAG_VLAN_ID_NOT_SET_ERROR_MESSAGE, tpName));
+                throw new VlanNotSetException(String.format(CTAG_VLAN_ID_NOT_SET_ERROR_MESSAGE, tpName));
+            }
+        } else {
+            String className = NrpCarrierEthConnectivityEndPointResource.class.toString();
+            LOG.warn(String.format(ATTRS_NOT_SET_ERROR_MESSAGE, tpName, className));
+            throw new ResourceNotAvailableException(String.format(ATTRS_NOT_SET_ERROR_MESSAGE, tpName, className));
+        }
+    }
+
+    /**
+     * Returns VLAN Id to be used internally in OvSwitch network
+     *
+     * @return Integer with VLAN Id
+     */
+    int getInternalVlanId() throws ResourceNotAvailableException {
+
+        return getCeVlanId();
+//		VlanUtils vlanUtils = new VlanUtils(nodes);
+//		Disable VLAN pool, refactor in the future
+//        if (vlanUtils.isVlanInUse(serviceVlanId)) {
+//            LOG.debug("VLAN ID = '" + serviceVlanId + "' already in use.");
+//            return vlanUtils.generateVlanID();
+//        } else {
+//            LOG.debug("VLAN ID = '" + serviceVlanId + "' not in use.");
+//            return serviceVlanId;
+//        }
+    }
+
+    /**
+     * Returns port name in openflow plugin naming convention (e.g. openflow:1:4)
+     *
+     * @return String with openflow port name
+     */
+    String getOpenFlowPortName() {
+        return portMap.get(tpName);
+    }
+
+    /**
+     * Returns port name for specifiec port name in openflow convention
+     * @param openFlowPortName port in openflow plugin naming convention
+     * @return String with port name
+     */
+    String getTpNameFromOpenFlowPortName(String openFlowPortName) {
+    	return portMap.inverse().get(openFlowPortName);
+    }
+
+    private BiMap<String, String> createPortMap(List<NullAwareDatastoreGetter<Node>> nodes) {
+    	BiMap<String, String> portMap = HashBiMap.create();
+        for (NullAwareDatastoreGetter<Node> node : nodes) {
+            if (node.get().isPresent()) {
+                for (NodeConnector nodeConnector : node.get().get().getNodeConnector()) {
+                    String ofName = nodeConnector.getId().getValue();
+                    FlowCapableNodeConnector flowCapableNodeConnector = nodeConnector.getAugmentation(FlowCapableNodeConnector.class);
+                    String name = flowCapableNodeConnector.getName();
+                    portMap.put(name, ofName);
+                }
+            }
+        }
+        return portMap;
+    }
+
+    protected static String getPortName(String sip) {
+        String[] tab = sip.split(":");
+        return tab[tab.length-1];
+    }
+
+	public long getQosMinRate() throws ResourceNotAvailableException {
+        IngressBwpFlow ingressBwpFlow = getIngressBwpFlow();
+        if(ingressBwpFlow != null) {
+            //TODO add validation
+            return ingressBwpFlow.getCir().getValue();
+        }
+
+		LOG.warn(String.format(INGRESS_BWP_FLOW_NOT_SET_ERROR_MESSAGE, tpName));
+        throw new ResourceNotAvailableException(String.format(INGRESS_BWP_FLOW_NOT_SET_ERROR_MESSAGE, tpName));
+
+	}
+
+	public long getQosMaxRate() throws ResourceNotAvailableException {
+
+        IngressBwpFlow ingressBwpFlow = getIngressBwpFlow();
+        if(ingressBwpFlow != null) {
+            //TODO add validation
+            return ingressBwpFlow.getCir().getValue() + ingressBwpFlow.getEir().getValue();
+        }
+
+        LOG.warn(String.format(INGRESS_BWP_FLOW_NOT_SET_ERROR_MESSAGE, tpName));
+        throw new ResourceNotAvailableException(String.format(INGRESS_BWP_FLOW_NOT_SET_ERROR_MESSAGE, tpName));
+	}
+
+    private IngressBwpFlow getIngressBwpFlow() {
+        if ( (endPoint.getAttrs() == null) || (endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource() == null) )
+            return null;
+        return endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource().getIngressBwpFlow();
+    }
+
+    protected boolean isIBwpConfigured() {
+        return getIngressBwpFlow() != null;
+    }
+}
